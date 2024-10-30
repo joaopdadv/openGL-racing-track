4998bf2e9547da25b23dc27ba2c200f505b8c0e5
joaopdadv <joaopdadv@gmail.com>
2024-10-30 11:53:11 -0300

    opengl cube setup

---
+++ src/Shader.cpp
@@ -0,0 +1,12 @@
+// #include "Shader.h"
+
+// Shader::Shader(const char *vertexPath, const char *fragmentPath, const char *geometryPath)
+// {
+//     // Constructor implementation
+//     // Load and compile shaders, handle geometry shader if provided
+// }
+
+// Shader::~Shader()
+// {
+//     // Destructor implementation (if needed)
+// }
