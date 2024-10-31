e7a842a5d08d926289ced63a00850ea353b6f61c
joaopdadv <joaopdadv@gmail.com>
2024-10-31 10:29:43 -0300

    remove shaders duplo

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -5,11 +5,8 @@ in vec2 TexCoord;
 
 // texture samplers
 uniform sampler2D texture1;
-uniform sampler2D texture2;
 
 void main()
 {
-	// linearly interpolate between both textures (80% container, 20% awesomeface)
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.5);
-	//FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+	FragColor = vec4(texture(texture1, TexCoord).rgb, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -134,7 +134,7 @@ int main()
 
 	// load and create a texture
 	// -------------------------
-	unsigned int texture1, texture2;
+	unsigned int texture1;
 	// texture 1
 	// ---------
 	glGenTextures(1, &texture1);
@@ -148,7 +148,7 @@ int main()
 	// load image, create texture and generate mipmaps
 	int width, height, nrChannels;
 	stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-	unsigned char *data = stbi_load("./images/track.png", &width, &height, &nrChannels, 0);
+	unsigned char *data = stbi_load("./images/pedra-28.jpg", &width, &height, &nrChannels, 0);
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -159,35 +159,11 @@ int main()
 		std::cout << "Failed to load texture aaaaa" << std::endl;
 	}
 	stbi_image_free(data);
-	// texture 2
-	// ---------
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	// set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	// load image, create texture and generate mipmaps
-	data = stbi_load("./images/opengl.png", &width, &height, &nrChannels, 0);
-	if (data)
-	{
-		// note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-	stbi_image_free(data);
 
 	// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
 	// -------------------------------------------------------------------------------------------
 	ourShader.use();
 	ourShader.setInt("texture1", 0);
-	ourShader.setInt("texture2", 1);
 
 	// render loop
 	// -----------
@@ -206,15 +182,13 @@ int main()
 		// bind textures on corresponding texture units
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture1);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture2);
 
 		// activate shader
 		ourShader.use();
 
 		// Camera
-		glm::vec3 cameraPos = glm::vec3(1.0f, 1.0f, 1.5f);
-		glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::vec3 cameraPos = glm::vec3(1.0f, 0.5f, 1.0f);
+		glm::vec3 cameraTarget = glm::vec3(0.0f, 0.25f, 0.0f);
 		glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
 		glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
 		glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
@@ -230,7 +204,7 @@ int main()
 		// model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.0f, -0.5f, 0.0f));
 		// model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 0.5f, -0.5f));
 
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, 0.0f));
+		// view = glm::translate(view, glm::vec3(0.0f, 0.0f, 0.0f));
 		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
 		// retrieve the matrix uniform locations
