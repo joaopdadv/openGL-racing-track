3dfb10e545004082dcc3690fef280095b2f8958a
João Pedro De Antoni De Villa <joaopedro@Joaos-MacBook-Air.local>
2024-10-31 13:11:49 -0300

    build.sh fixes macbook

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -xe
 mkdir -p build/Debug
 
 # arch64
-clang++ -g -std=c++17 -Iinclude src/main.cpp src/Shader.cpp src/glad.c -lglfw -framework OpenGL -o build/Debug/outDebug
+clang++ -g -std=c++17 -Iinclude -I/opt/homebrew/include -L/opt/homebrew/lib src/main.cpp src/Shader.cpp src/glad.c -lglfw -framework OpenGL -o build/Debug/outDebug
 
 ./build/Debug/outDebug
